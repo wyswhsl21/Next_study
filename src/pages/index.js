b51d19c77c9677314b7751b56f3353c34cf406ec
DESKTOP-QR42JKC\wyswh <wyswhsl21@naver.com>
2024-05-18 12:50:39 +0900

    넥스트에서 ssr 코드 작성하는 방법 , client 코드 작성하는 방법 공부

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,44 +6,21 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-  const code = "KOR";
-  const router = useRouter();
-  const onClickHandler = () => {
-    router.push({
-      pathname: "/country/[code]",
-      query: { code: code },
-    });
-  };
+export default function Home({ name }) {
   /**
    * router push 페이지 이동
    * router replace 새로고침 안하고 뒤로가기
    * router back 새로고침 하면서 뒤로가기
    * router reload 새로고침
-   * 
+   *
    */
-  return (
-    <div>
-      <div>
-        <button onClick={() => onClickHandler()}>Search 페이지 이동</button>
-      </div>
-      <div>
-        <Link href={"/search"}>Search Page 이동</Link>
-      </div>
-
-      <div>
-        {/* url object 를 이용하여 구조화 하는것도 가능 
-        아니면 href="/country/search" 처럼 경로를 지정해주는것도 가능
-        */}
-        <Link
-          href={{
-            pathname: "/country/[code]",
-            query: { code: code },
-          }}
-        >
-          {code} 페이지 이동
-        </Link>
-      </div>
-    </div>
-  );
+  return <div>{name}</div>;
 }
+//SSR 을 위해 서버 측에서 컴포넌트에게 전달할 데이터를 설정하는 함수
+export const getServerSideProps = async () => {
+  return {
+    props: {
+      name: "KOREA",
+    },
+  };
+};
